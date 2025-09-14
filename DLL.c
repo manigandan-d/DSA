8a372ad016a8f02c0c7720e0243360631afab2e5
manigandan-d <manigandan220916@gmail.com>
2025-09-14 19:47:50 +0530

    doubly linked list

--- DLL.c
+++ DLL.c
@@ -21,7 +21,33 @@ void delete_at_pos();
 int main() {
     insert_at_beg();
     insert_at_end();
+    insert_at_end();
+    insert_at_end();
 
+    print_list();
+    printf("Length: %d\n", length());
+
+    insert_at_pos();
+    print_list();
+    printf("Length: %d\n", length());
+
+    insert_at_pos();
+    print_list();
+    printf("Length: %d\n", length());
+
+    insert_at_pos();
+    print_list();
+    printf("Length: %d\n", length());
+
+    delete_at_beg();
+    print_list();
+    printf("Length: %d\n", length());
+
+    delete_at_end();
+    print_list();
+    printf("Length: %d\n", length());
+
+    delete_at_pos();
     print_list();
     printf("Length: %d\n", length());
 
@@ -70,19 +96,133 @@ void insert_at_end() {
 }
 
 void insert_at_pos() {
+    int pos, len, index = 1; 
 
+    printf("Enter the position to insert: ");
+    scanf("%d", &pos);
+
+    len = length();
+
+    if(pos < 1 || pos > len+1) {
+        printf("Invalid position\n");
+        return; 
+    }
+
+    struct Node *new = (struct Node *)malloc(sizeof(struct Node));
+    new->prev = NULL;
+    new->next = NULL;
+
+    printf("Enter the value: ");
+    scanf("%d", &new->data);
+
+    if(pos == 1) {
+        if(head != NULL) {
+            head->prev = new; 
+            new->next = head;
+        }
+
+        head = new; 
+
+        return; 
+    }
+
+    struct Node *temp = head; 
+
+    while(index < pos-1) {
+        index++;
+        temp = temp->next; 
+    }
+
+    if(temp->next != NULL) {
+        new->next = temp->next;
+        temp->next->prev = new; 
+    }
+
+    new->prev = temp;
+    temp->next = new; 
 }
 
 void delete_at_beg() {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
 
+    struct Node *temp = head;
+    head = head->next;
+
+    if (head != NULL) {
+        head->prev = NULL;
+    }
+
+    free(temp);
 }
 
 void delete_at_end() {
+    if (head == NULL) {
+        printf("List is empty\n");
+        return;
+    }
 
+    struct Node *temp = head;
+
+    if (head->next == NULL) {  
+        head = NULL;
+        free(temp);
+        return;
+    }
+
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+
+    temp->prev->next = NULL;
+    free(temp);
 }
 
 void delete_at_pos() {
-    
+    int pos, len, index = 1;
+
+    printf("Enter the position to delete: ");
+    scanf("%d", &pos);
+
+    len = length();
+
+    if (pos < 1 || pos > len) {
+        printf("Invalid position\n");
+        return;
+    }
+
+    struct Node *temp = head; 
+
+    if (pos == 1) {
+        head = head->next;
+
+        if (head != NULL) {
+            head->prev = NULL;
+        }
+
+        free(temp);
+
+        return;
+    }
+
+    temp = head;
+
+    while (index < pos) {
+        index++;
+        temp = temp->next;
+    }
+
+    if (temp->next != NULL) {
+        temp->next->prev = temp->prev;
+    }
+
+    if (temp->prev != NULL) {
+        temp->prev->next = temp->next;
+    }
+
+    free(temp);
 }
 
 void print_list() {
